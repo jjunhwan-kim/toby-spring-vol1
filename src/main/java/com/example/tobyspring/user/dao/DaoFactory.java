48ee8ab0e763d03717dd3708cb7b967d057ce742
nefertirii <nefertiri972@gmail.com>
2021-04-23 01:54:39 +0900

    Add DaoFactory
    UserDao와 ConnectionMaker 구현체를 생성하는 역할을 하는  DaoFactory 클래스 생성
    UserDaoTest는 DaoFactory로 부터 UserDao 오브젝트를 전달받아 테스트하는 역할만 수행

---
+++ src/main/java/com/example/tobyspring/user/dao/DaoFactory.java
@@ -0,0 +1,9 @@
+package com.example.tobyspring.user.dao;
+
+public class DaoFactory {
+    public UserDao userDao() {
+        ConnectionMaker connectionMaker = new DConnectionMaker();
+        UserDao userDao = new UserDao(connectionMaker);
+        return userDao;
+    }
+}
--- src/test/java/com/example/tobyspring/user/UserDaoTest.java
+++ src/test/java/com/example/tobyspring/user/UserDaoTest.java
@@ -2,6 +2,7 @@ package com.example.tobyspring.user;
 
 import com.example.tobyspring.user.dao.ConnectionMaker;
 import com.example.tobyspring.user.dao.DConnectionMaker;
+import com.example.tobyspring.user.dao.DaoFactory;
 import com.example.tobyspring.user.dao.UserDao;
 import com.example.tobyspring.user.domain.User;
 import org.junit.jupiter.api.Test;
@@ -14,20 +15,19 @@ class UserDaoTest {
 
     @Test
     public void userDaoTest() throws SQLException, ClassNotFoundException {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-
-        UserDao dao = new UserDao(connectionMaker);
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.userDao();
 
         User user = new User();
         user.setId("whiteship");
         user.setName("백기선");
         user.setPassword("married");
 
-        dao.add(user);
+        userDao.add(user);
 
         System.out.println(user.getId() + " 등록 성공");
 
-        User user2 = dao.get(user.getId());
+        User user2 = userDao.get(user.getId());
         System.out.println(user2.getName());
         System.out.println(user2.getPassword());
         System.out.println(user2.getId() + " 조회 성공");
