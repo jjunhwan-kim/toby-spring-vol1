68b1d1890bf68c1cabb29d1c6166ec32ee46f0e7
nefertirii <nefertiri972@gmail.com>
2021-04-22 22:31:35 +0900

    Remove duplicate code
    getConnection 메소드를 만들어 커넥션을 가져오는 중복코드 제거

--- src/main/java/com/example/tobyspring/user/UserDao.java
+++ src/main/java/com/example/tobyspring/user/UserDao.java
@@ -6,8 +6,7 @@ import java.sql.*;
 
 public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("org.h2.Driver");
-        Connection c = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa","");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
         ps.setString(1, user.getId());
@@ -21,8 +20,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("org.h2.Driver");
-        Connection c = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa","");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
@@ -39,5 +37,11 @@ public class UserDao {
         c.close();
 
         return user;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection c = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa","");
+        return c;
     }
 }
