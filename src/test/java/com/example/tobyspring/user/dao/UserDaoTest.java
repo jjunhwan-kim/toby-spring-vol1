00b0c1c9fb4a0cedfd8d71e388cf2758956beb19
nefertirii <nefertiri972@gmail.com>
2021-04-26 05:47:36 +0900

    Modify test code
    addAndGet 테스트 메소드 코드 보완

--- src/test/java/com/example/tobyspring/user/dao/UserDaoTest.java
+++ src/test/java/com/example/tobyspring/user/dao/UserDaoTest.java
@@ -18,22 +18,26 @@ class UserDaoTest {
     public void addAndGet() throws SQLException {
 //        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        UserDao userDao = context.getBean("userDao", UserDao.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
-        userDao.deleteAll();
-        assertThat(userDao.getCount()).isEqualTo(0);
+        User user1 = new User("gyumee", "박성철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
 
-        User user = new User("gyumee", "박성철", "springno1");
+        dao.deleteAll();
+        assertThat(dao.getCount()).isEqualTo(0);
 
-        userDao.add(user);
+        dao.add(user1);
+        dao.add(user2);
 
-        assertThat(userDao.getCount()).isEqualTo(1);
+        assertThat(dao.getCount()).isEqualTo(2);
 
-        User user2 = userDao.get(user.getId());
+        User userget1 = dao.get(user1.getId());
+        assertThat(userget1.getName()).isEqualTo(user1.getName());
+        assertThat(userget1.getPassword()).isEqualTo(user1.getPassword());
 
-        assertThat(user.getId()).isEqualTo(user2.getId());
-        assertThat(user.getName()).isEqualTo(user2.getName());
-        assertThat(user.getPassword()).isEqualTo(user2.getPassword());
+        User userget2 = dao.get(user2.getId());
+        assertThat(userget2.getName()).isEqualTo(user2.getName());
+        assertThat(userget2.getPassword()).isEqualTo(user2.getPassword());
     }
 
     @Test
