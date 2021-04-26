7dd5fe6acac971da535e661cd7c7bb4d0f22078f
nefertirii <nefertiri972@gmail.com>
2021-04-27 00:10:55 +0900

    Remove spring container
    스프링 컨테이너를 사용하지 않고 직접 오브젝트를 만들어 DI 해서 사용하도록 변경

--- src/test/java/com/example/tobyspring/user/dao/UserDaoTest.java
+++ src/test/java/com/example/tobyspring/user/dao/UserDaoTest.java
@@ -1,35 +1,18 @@
 package com.example.tobyspring.user.dao;
 
 import com.example.tobyspring.user.domain.User;
-import org.assertj.core.api.Assertions;
-import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(locations = "/test-applicationContext.xml")
-//@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD) // 테스트 메소드에서 애플리케이션 컨텍스트의 구성이나 상태를 변경한다는 것을 테스트 컨텍스트 프레임워크에 알려줌
-//@ContextConfiguration(classes = DaoFactory.class)
 class UserDaoTest {
-    @Autowired
     private UserDao dao;
     private User user1;
     private User user2;
@@ -37,6 +20,10 @@ class UserDaoTest {
 
     @BeforeEach
     public void setUp() {
+        dao = new UserDao();
+        DataSource dataSource = new SingleConnectionDataSource("jdbc:h2:tcp://localhost/~/test", "sa", "", true);
+        dao.setDataSource(dataSource);
+
         user1 = new User("gyumee", "박성철", "springno1");
         user2 = new User("leegw700", "이길원", "springno2");
         user3 = new User("bumjin", "박범진", "springno3");
