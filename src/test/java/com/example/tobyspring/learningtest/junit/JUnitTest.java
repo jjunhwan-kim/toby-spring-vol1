8da3aab2ffdc1d8688c09c5498f855a1cce1dd4c
nefertirii <nefertiri972@gmail.com>
2021-04-27 00:26:31 +0900

    Update JUnitTest
    기존 테스트는 직전 테스트에서 만들어진 테스트 오브젝트와만 비교하므로 첫 번째와 세 번째 테스트 오브젝트가 같은 경우가 있다면 검증이 안됨
    따라서 중복되지 않는 Set 자료구조에 테스트 오브젝트를 저장하여 비교

--- src/test/java/com/example/tobyspring/learningtest/junit/JUnitTest.java
+++ src/test/java/com/example/tobyspring/learningtest/junit/JUnitTest.java
@@ -1,26 +1,38 @@
 package com.example.tobyspring.learningtest.junit;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class JUnitTest {
-    static JUnitTest testObject;
-    
+    static Set<JUnitTest> testObjects = new HashSet<>();
+
+    @BeforeEach
+    public void setUp() {
+        System.out.println("testObjects = " + testObjects);
+    }
+
     @Test
     public void test1() {
-        Assertions.assertThat(this).isNotSameAs(testObject);
-        testObject = this;
+        Assertions.assertThat(testObjects).doesNotContain(this);
+        testObjects.add(this);
+        System.out.println(this);
     }
 
     @Test
     public void test2() {
-        Assertions.assertThat(this).isNotSameAs(testObject);
-        testObject = this;
+        Assertions.assertThat(testObjects).doesNotContain(this);
+        testObjects.add(this);
+        System.out.println(this);
     }
 
     @Test
     public void test3() {
-        Assertions.assertThat(this).isNotSameAs(testObject);
-        testObject = this;
+        Assertions.assertThat(testObjects).doesNotContain(this);
+        testObjects.add(this);
+        System.out.println(this);
     }
 }
