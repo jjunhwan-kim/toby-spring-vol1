f5facf552d3735afea4ab2808f08245beea6bc36
nefertirii <nefertiri972@gmail.com>
2021-04-27 01:01:28 +0900

    Add learning test
    스프링 테스트 컨텍스트 프레임워크 학습테스트 추가
    테스트 개수에 상관없이 컨텍스트가 한 개만 만들어 지는지 검증

--- src/test/java/com/example/tobyspring/learningtest/junit/JUnitTest.java
+++ src/test/java/com/example/tobyspring/learningtest/junit/JUnitTest.java
@@ -1,14 +1,30 @@
 package com.example.tobyspring.learningtest.junit;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = JUnitTest.TestConfig.class)
 public class JUnitTest {
+    @Autowired
+    ApplicationContext context;
+
     static Set<JUnitTest> testObjects = new HashSet<>();
+    static ApplicationContext contextObject = null;
 
     @BeforeEach
     public void setUp() {
@@ -17,22 +33,36 @@ public class JUnitTest {
 
     @Test
     public void test1() {
-        Assertions.assertThat(testObjects).doesNotContain(this);
+        assertThat(testObjects).doesNotContain(this);
         testObjects.add(this);
         System.out.println(this);
+        
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
     }
 
     @Test
     public void test2() {
-        Assertions.assertThat(testObjects).doesNotContain(this);
+        assertThat(testObjects).doesNotContain(this);
         testObjects.add(this);
         System.out.println(this);
+
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 
     @Test
     public void test3() {
-        Assertions.assertThat(testObjects).doesNotContain(this);
+        assertThat(testObjects).doesNotContain(this);
         testObjects.add(this);
         System.out.println(this);
+
+        assertThat(contextObject, either(is(nullValue())).or(is(this.context)));
+        contextObject = this.context;
+    }
+
+    @Configuration
+    static class TestConfig {
+
     }
 }
