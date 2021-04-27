16e6f27071942e95506fb2eb3a76d6ee5ca098e2
nefertirii <nefertiri972@gmail.com>
2021-04-28 01:40:27 +0900

    Change local class to anonymous inner class
    AddStatement 와 DeleteAllStatement 클래스를 익명 내부 클래스로 변환

--- src/main/java/com/example/tobyspring/user/dao/UserDao.java
+++ src/main/java/com/example/tobyspring/user/dao/UserDao.java
@@ -20,7 +20,7 @@ public class UserDao {
 */
 
     public void add(User user) throws SQLException {
-        class AddStatement implements StatementStrategy {
+        jdbcContextWithStatementStrategy(new StatementStrategy() {
             @Override
             public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
                 PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
@@ -31,10 +31,7 @@ public class UserDao {
 
                 return ps;
             }
-        }
-
-        StatementStrategy st = new AddStatement();
-        jdbcContextWithStatementStrategy(st);
+        });
     }
 
     public User get(String id) throws SQLException {
@@ -90,8 +87,13 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        StatementStrategy st = new DeleteAllStatement();
-        jdbcContextWithStatementStrategy(st);
+        jdbcContextWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("delete from users");
+                return ps;
+            }
+        });
     }
 
     public int getCount() throws SQLException {
