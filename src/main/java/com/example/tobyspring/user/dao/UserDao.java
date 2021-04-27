552bde9ab5359c34fb8052b3b35670e6a23db61b
nefertirii <nefertiri972@gmail.com>
2021-04-28 01:42:48 +0900

    Change anonymous inner class to lambda expression
    jdbcContextWithStatementStrategy 메소드로 전달되는 익명 내부 클래스를 람다식으로 변환

--- src/main/java/com/example/tobyspring/user/dao/UserDao.java
+++ src/main/java/com/example/tobyspring/user/dao/UserDao.java
@@ -20,17 +20,14 @@ public class UserDao {
 */
 
     public void add(User user) throws SQLException {
-        jdbcContextWithStatementStrategy(new StatementStrategy() {
-            @Override
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+        jdbcContextWithStatementStrategy(c -> {
+            PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
 
-                ps.setString(1, user.getId());
-                ps.setString(2, user.getName());
-                ps.setString(3, user.getPassword());
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
 
-                return ps;
-            }
+            return ps;
         });
     }
 
@@ -87,12 +84,8 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        jdbcContextWithStatementStrategy(new StatementStrategy() {
-            @Override
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("delete from users");
-                return ps;
-            }
+        jdbcContextWithStatementStrategy(c -> {
+            return c.prepareStatement("delete from users");
         });
     }
 
