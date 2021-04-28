8c7737790794ee108af20d81341e1084fcb368c7
nefertirii <nefertiri972@gmail.com>
2021-04-28 19:22:06 +0900

    Use manual dependency injection
    JdbcContext 오브젝트를 스프링 빈에서 제거하고, UserDao 에서 직접 생성하여 DataSource 를 수동 DI 하도록 수정
    UserDao에서 직접 생성하기 때문에 싱글톤으로는 사용 불가능하고 Dao 마다 새로 생성해야 함

--- src/main/java/com/example/tobyspring/user/dao/UserDao.java
+++ src/main/java/com/example/tobyspring/user/dao/UserDao.java
@@ -10,11 +10,11 @@ public class UserDao {
     private DataSource dataSource;
     private JdbcContext jdbcContext;
 
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
-    }
-
     public void setDataSource(DataSource dataSource) {
+        this.jdbcContext = new JdbcContext();
+
+        this.jdbcContext.setDataSource(dataSource);
+        
         this.dataSource = dataSource;
     }
 
