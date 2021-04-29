c46e035030100873e3141ce252ca9c3d564135cb
nefertirii <nefertiri972@gmail.com>
2021-04-29 19:10:14 +0900

    Use template/callback pattern
    템플릿 메소드 에서는 콜백 오브젝트에게 BufferedReader를 넘겨주면서 콜백을 호출, 예외처리 루틴을 수행
    콜백에서는 BufferedReader를 이용해 덧셈, 곱셈 계산
    calcSum과 calcMultiply 메소드는 클라이언트 역할로 익명 내부클래스 콜백을 생성하여 템플릿에게 전달

--- src/test/java/com/example/tobyspring/learningtest/template/CalcSumTest.java
+++ src/test/java/com/example/tobyspring/learningtest/template/CalcSumTest.java
@@ -1,6 +1,7 @@
 package com.example.tobyspring.learningtest.template;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -8,10 +9,22 @@ import java.io.IOException;
 import static org.assertj.core.api.Assertions.*;
 
 public class CalcSumTest {
+    Calculator calculator;
+    String numFilepath;
+
+    @BeforeEach
+    public void setUp() {
+        this.calculator = new Calculator();
+        this.numFilepath = getClass().getResource("/numbers.txt").getPath();
+    }
+
     @Test
     public void sumOfNumbers() throws IOException {
-        Calculator calculator = new Calculator();
-        int sum = calculator.calcSum(getClass().getResource("/numbers.txt").getPath());
-        assertThat(sum).isEqualTo(15);
+        assertThat(calculator.calcSum(this.numFilepath)).isEqualTo(15);
+    }
+
+    @Test
+    public void multiplyOfNumbers() throws IOException {
+        assertThat(calculator.calcMultiply(this.numFilepath)).isEqualTo(120);
     }
 }
