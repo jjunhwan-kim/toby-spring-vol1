20c2d9d6b3a04e864df945126239459b73a2fb05
nefertirii <nefertiri972@gmail.com>
2021-04-30 00:25:49 +0900

    Improve UserDao
    RowMapper 익명 내부 클래스가 중복되므로 인스턴스 변수로 수정
    DataSource 인스턴스 변수 삭제

--- src/main/java/com/example/tobyspring/user/dao/UserDao.java
+++ src/main/java/com/example/tobyspring/user/dao/UserDao.java
@@ -12,8 +12,18 @@ import java.sql.*;
 import java.util.List;
 
 public class UserDao {
-    private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
+
+    private RowMapper<User> userMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+            return user;
+        }
+    };
 
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
@@ -24,65 +34,20 @@ public class UserDao {
     }
 
     public User get(String id) throws SQLException {
-        return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-                new RowMapper<User>() {
-                    @Override
-                    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        User user = new User();
-                        user.setId(rs.getString("id"));
-                        user.setName(rs.getString("name"));
-                        user.setPassword(rs.getString("password"));
-                        return user;
-                    }
-                }, id);
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?", this.userMapper, id);
     }
 
     public void deleteAll() throws SQLException {
-        /*
-        this.jdbcTemplate.update(new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-                return con.prepareStatement("delete from users");
-            }
-        });
-        */
         this.jdbcTemplate.update("delete from users");
     }
 
     public int getCount() throws SQLException {
-        /*
-        return this.jdbcTemplate.query(
-            new PreparedStatementCreator() {
-                @Override
-                public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-                    return con.prepareStatement("select count(*) from users");
-                }
-            },
-            new ResultSetExtractor<Integer>() {
-                @Override
-                public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
-                    rs.next();
-                    return rs.getInt(1);
-                }
-            }
-        );
-        */
         Integer value = this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
         if (value == null) return 0;
         return value;
     }
 
     public List<User> getAll() {
-        return this.jdbcTemplate.query("select * from users order by id",
-                new RowMapper<User>() {
-                    @Override
-                    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        User user = new User();
-                        user.setId(rs.getString("id"));
-                        user.setName(rs.getString("name"));
-                        user.setPassword(rs.getString("password"));
-                        return user;
-                    }
-                });
+        return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
     }
 }
