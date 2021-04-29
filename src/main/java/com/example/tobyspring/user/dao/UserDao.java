53444784406ec3420c9162742407cef8fcb9777d
nefertirii <nefertiri972@gmail.com>
2021-04-29 23:34:27 +0900

    Change JdbcContext to JdbcTemplate
    JdbcContext 에서 JdbcTemplate 으로 변경

--- src/main/java/com/example/tobyspring/user/dao/UserDao.java
+++ src/main/java/com/example/tobyspring/user/dao/UserDao.java
@@ -1,64 +1,73 @@
 package com.example.tobyspring.user.dao;
 
 import com.example.tobyspring.user.domain.User;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
     private DataSource dataSource;
-    private JdbcContext jdbcContext;
+    private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
-        this.jdbcContext = new JdbcContext();
-
-        this.jdbcContext.setDataSource(dataSource);
-
-        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public void add(User user) throws SQLException {
-        this.jdbcContext.workWithStatementStrategy(c -> {
-            PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-
-            ps.setString(1, user.getId());
-            ps.setString(2, user.getName());
-            ps.setString(3, user.getPassword());
-
-            return ps;
-        });
+        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
     }
 
     public User get(String id) throws SQLException {
-        return this.jdbcContext.workWithStatementStrategyAndRowMapper(
-            c -> {
-                PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-                ps.setString(1, id);
-
-                return ps;
-            },
-            rs -> {
-                User user;
-                user = new User();
-                user.setId(rs.getString("id"));
-                user.setName(rs.getString("name"));
-                user.setPassword(rs.getString("password"));
-                return user;
-            }
-        );
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?",
+            new RowMapper<User>() {
+                @Override
+                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    User user = new User();
+                    user.setId(rs.getString("id"));
+                    user.setName(rs.getString("name"));
+                    user.setPassword(rs.getString("password"));
+                    return user;
+                }
+            }, id);
     }
 
     public void deleteAll() throws SQLException {
-        this.jdbcContext.executeSql("delete from users");
+        /*
+        this.jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                return con.prepareStatement("delete from users");
+            }
+        });
+        */
+        this.jdbcTemplate.update("delete from users");
     }
 
     public int getCount() throws SQLException {
-        return this.jdbcContext.workWithStatementStrategyAndRowMapper(
-            c -> {
-                PreparedStatement ps = c.prepareStatement("select count(*) from users");
-                return ps;
+        /*
+        return this.jdbcTemplate.query(
+            new PreparedStatementCreator() {
+                @Override
+                public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                    return con.prepareStatement("select count(*) from users");
+                }
             },
-            rs -> rs.getInt(1)
+            new ResultSetExtractor<Integer>() {
+                @Override
+                public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+                    rs.next();
+                    return rs.getInt(1);
+                }
+            }
         );
+        */
+        Integer value = this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+        if (value == null) return 0;
+        return value;
     }
 }
