bebc37728f166c2f3776aadaf2d6ae61ef2d4c95
nefertirii <nefertiri972@gmail.com>
2021-05-01 17:09:25 +0900

    Improve user update test
    User update 테스트 보완
    기존 테스트는 update 에서 where 구문을 누락했을 때도 통과됨(왜냐하면 where 구문을 누락하면 모든 유저를 수정하기 때문)
    따라서 두 명의 사용자를 추가하고 그중 한 명의 사용자만 수정하고 두 사용자의 값을 비교하도록 수정함

--- src/test/java/com/example/tobyspring/user/dao/UserDaoTest.java
+++ src/test/java/com/example/tobyspring/user/dao/UserDaoTest.java
@@ -150,6 +150,7 @@ class UserDaoTest {
         dao.deleteAll();
 
         dao.add(user1);
+        dao.add(user2);
 
         user1.setName("오민규");
         user1.setPassword("springno6");
@@ -160,5 +161,7 @@ class UserDaoTest {
 
         User user1update = dao.get(user1.getId());
         checkSameUser(user1, user1update);
+        User user2same = dao.get(user2.getId());
+        checkSameUser(user2, user2same);
     }
 }
