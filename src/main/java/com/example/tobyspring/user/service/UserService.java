6bb75d1959390b1788b7ca06dd80363fbcd2d825
nefertirii <nefertiri972@gmail.com>
2021-05-02 00:35:10 +0900

    Add UserService add
    UserService 에 add 메소드 추가
    처음 가입하는 사용자의 레벨을 Basic 으로 초기화

--- src/main/java/com/example/tobyspring/user/service/UserService.java
+++ src/main/java/com/example/tobyspring/user/service/UserService.java
@@ -37,4 +37,9 @@ public class UserService {
             }
         }
     }
+
+    public void add(User user) {
+        if (user.getLevel() == null) user.setLevel(Level.BASIC);
+        userDao.add(user);
+    }
 }
--- src/test/java/com/example/tobyspring/user/service/UserServiceTest.java
+++ src/test/java/com/example/tobyspring/user/service/UserServiceTest.java
@@ -57,7 +57,21 @@ class UserServiceTest {
     }
 
     @Test
-    public void bean() {
-        assertThat(userService).isNotNull();
+    public void add() {
+        userDao.deleteAll();
+
+        User userWithLevel = users.get(4);
+        User userWithoutLevel = users.get(0);
+        userWithoutLevel.setLevel(null);
+
+        userService.add(userWithLevel);
+        userService.add(userWithoutLevel);
+
+        User userWithLevelRead = userDao.get(userWithLevel.getId());
+        User userWithoutLevelRead = userDao.get(userWithoutLevel.getId());
+
+        assertThat(userWithLevelRead.getLevel()).isEqualTo(userWithLevel.getLevel());
+        assertThat(userWithoutLevelRead.getLevel()).isEqualTo(Level.BASIC);
     }
+
 }
