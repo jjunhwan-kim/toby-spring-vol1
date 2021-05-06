1dcfe36459b30e51768df46cd015081dbb6a1ba8
nefertirii <nefertiri972@gmail.com>
2021-05-06 23:38:47 +0900

    Add transaction interface
    스프링의 트랜잭션 추상화 API 적용
    PlatformTransactionManager 인터페이스의 구현체로 DataSourceTransactionManager 사용

--- src/main/java/com/example/tobyspring/user/service/UserService.java
+++ src/main/java/com/example/tobyspring/user/service/UserService.java
@@ -3,12 +3,12 @@ package com.example.tobyspring.user.service;
 import com.example.tobyspring.user.dao.UserDao;
 import com.example.tobyspring.user.domain.Level;
 import com.example.tobyspring.user.domain.User;
-import org.springframework.jdbc.datasource.DataSourceUtils;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserService {
@@ -26,10 +26,10 @@ public class UserService {
         this.userDao = userDao;
     }
 
-    public void upgradeLevels() throws Exception {
-        TransactionSynchronizationManager.initSynchronization();
-        Connection c = DataSourceUtils.getConnection(dataSource);
-        c.setAutoCommit(false);
+    public void upgradeLevels() {
+        PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
+
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
 
         try {
             List<User> users = userDao.getAll();
@@ -39,16 +39,11 @@ public class UserService {
                     upgradeLevel(user);
                 }
             }
-            c.commit();
+            transactionManager.commit(status);
         }
         catch (Exception e) {
-            c.rollback();
+            transactionManager.rollback(status);
             throw e;
-        }
-        finally {
-            DataSourceUtils.releaseConnection(c, dataSource);
-            TransactionSynchronizationManager.unbindResource(this.dataSource);
-            TransactionSynchronizationManager.clearSynchronization();
         }
     }
 
