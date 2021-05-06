90c6da4c810ffe7313c89fde46b15aa98b52962c
nefertirii <nefertiri972@gmail.com>
2021-05-07 01:18:35 +0900

    Add MailSender interface
    메일 발송 기능 추상화

--- src/main/java/com/example/tobyspring/user/service/UserService.java
+++ src/main/java/com/example/tobyspring/user/service/UserService.java
@@ -3,20 +3,14 @@ package com.example.tobyspring.user.service;
 import com.example.tobyspring.user.dao.UserDao;
 import com.example.tobyspring.user.domain.Level;
 import com.example.tobyspring.user.domain.User;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.io.UnsupportedEncodingException;
 import java.util.List;
-import java.util.Properties;
 
 public class UserService {
     public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
@@ -24,6 +18,7 @@ public class UserService {
 
     private PlatformTransactionManager transactionManager;
     private UserDao userDao;
+    private MailSender mailSender;
 
     public void setTransactionManager(PlatformTransactionManager transactionManager) {
         this.transactionManager = transactionManager;
@@ -31,6 +26,10 @@ public class UserService {
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public void setMailSender(MailSender mailSender) {
+        this.mailSender = mailSender;
     }
 
     public void upgradeLevels() {
@@ -59,25 +58,13 @@ public class UserService {
     }
 
     private void sendUpgradeEmail(User user) {
-        Properties props = new Properties();
-        props.put("mail.stmp.host", "mail.ksug.org");
-        Session s = Session.getInstance(props, null);
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(user.getEmail());
+        mailMessage.setFrom("useradmin@ksug.org");
+        mailMessage.setSubject("Upgrade 안내");
+        mailMessage.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드되었습니다");
 
-        MimeMessage message = new MimeMessage(s);
-        try {
-            message.setFrom(new InternetAddress("useradmin@ksug.org"));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
-            message.setSubject("Upgrade 안내");
-            message.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드되었습니다");
-
-            Transport.send(message);
-        }
-        catch (AddressException e) {
-            throw new RuntimeException();
-        }
-        catch (MessagingException e) {
-            throw new RuntimeException();
-        }
+        this.mailSender.send(mailMessage);
     }
 
     private boolean canUpgradeLevel(User user) {
