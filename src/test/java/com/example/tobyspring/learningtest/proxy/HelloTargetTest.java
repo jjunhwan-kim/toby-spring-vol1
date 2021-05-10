e9c5fc1f9e17e2f23875143288e64b5c30027484
nefertirii <nefertiri972@gmail.com>
2021-05-10 23:29:48 +0900

    Add learning test
    리플렉션 API 중 Method 인터페이스를 이용하여 메소드를 호출하는 테스트
    다이내믹 프록시를 적용하기 위해 프록시 인터페이스 및 클래스 추가

---
+++ src/test/java/com/example/tobyspring/learningtest/proxy/HelloTargetTest.java
@@ -0,0 +1,22 @@
+package com.example.tobyspring.learningtest.proxy;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTargetTest {
+    @Test
+    public void simpleProxy() {
+        Hello hello = new HelloTarget();
+        assertThat(hello.sayHello("Toby")).isEqualTo("Hello Toby");
+        assertThat(hello.sayHi("Toby")).isEqualTo("Hi Toby");
+        assertThat(hello.sayThankYou("Toby")).isEqualTo("Thank You Toby");
+
+        Hello proxiedHello = new HelloUppercase(new HelloTarget());
+        assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+        assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+    }
+}
