744b35eefcdb1769949aaf389512b2f98575c011
nefertirii <nefertiri972@gmail.com>
2021-05-11 23:44:20 +0900

    Add dynamic proxy
    다이나믹 프록시 적용
    InvocationHandler 인터페이스를 구현한 오브젝트를 다이내믹 프록시에게 제공하면, 다이내믹 프록시가 받은 모든 요청을 invocationHandler의 invoke() 메소드로 보내줌

--- src/test/java/com/example/tobyspring/learningtest/proxy/HelloTargetTest.java
+++ src/test/java/com/example/tobyspring/learningtest/proxy/HelloTargetTest.java
@@ -3,6 +3,8 @@ package com.example.tobyspring.learningtest.proxy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Proxy;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +17,14 @@ class HelloTargetTest {
         assertThat(hello.sayThankYou("Toby")).isEqualTo("Thank You Toby");
 
         Hello proxiedHello = new HelloUppercase(new HelloTarget());
+        assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+        assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+    }
+
+    @Test
+    public void dynamicProxy() {
+        Hello proxiedHello = (Hello)Proxy.newProxyInstance(getClass().getClassLoader(), new Class[] {Hello. class}, new UppercaseHandler(new HelloTarget()));
         assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
         assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
         assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
