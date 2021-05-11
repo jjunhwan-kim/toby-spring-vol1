87cc0ec0c47f84f9c60840f2c7bb1f7d38eeb09e
nefertirii <nefertiri972@gmail.com>
2021-05-12 00:14:07 +0900

    Add dynamic proxy for transaction
    UserServiceTx 를 다이내믹 프록시 방식으로 변경
    TransactionHandler 생성
    다이내믹 프록시 오브젝트 생성

--- src/test/java/com/example/tobyspring/user/service/UserServiceTest.java
+++ src/test/java/com/example/tobyspring/user/service/UserServiceTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -155,9 +156,12 @@ class UserServiceTest {
         testUserService.setUserDao(userDao);
         testUserService.setMailSender(mailSender);
 
-        UserServiceTx txUserService = new UserServiceTx();
-        txUserService.setTransactionManager(transactionManager);
-        txUserService.setUserService(testUserService);
+        TransactionHandler txHandler = new TransactionHandler();
+        txHandler.setTarget(testUserService);
+        txHandler.setTransactionManager(transactionManager);
+        txHandler.setPattern("upgradeLevels");
+
+        UserService txUserService = (UserService)Proxy.newProxyInstance(getClass().getClassLoader(), new Class[] {UserService.class}, txHandler);
 
         userDao.deleteAll();
         for (User user : users) userDao.add(user);
