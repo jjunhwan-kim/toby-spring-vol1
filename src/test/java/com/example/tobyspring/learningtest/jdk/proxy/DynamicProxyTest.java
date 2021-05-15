adc994f439905961e69b3d87884193eb632eeb31
nefertirii <nefertiri972@gmail.com>
2021-05-15 23:27:36 +0900

    Add ProxyFactoryBean learning test
    스프링의 ProxyFactoryBean을 이용한 다이내믹 프록시 테스트 코드 추가
    MethodInterceptor는 InvocationHandler와 달리 타깃 오브젝트에 대한 정보를 제공받음(ProxyFactoryBean으로 부터 전달받음)
    따라서 타깃 오브젝트에 상관없이 독립적으로 만들어 질 수 있음

---
+++ src/test/java/com/example/tobyspring/learningtest/jdk/proxy/DynamicProxyTest.java
@@ -0,0 +1,120 @@
+package com.example.tobyspring.learningtest.jdk.proxy;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DynamicProxyTest {
+    @Test
+    public void simpleProxy() {
+        Hello hello = new HelloTarget();
+        assertThat(hello.sayHello("Toby")).isEqualTo("Hello Toby");
+        assertThat(hello.sayHi("Toby")).isEqualTo("Hi Toby");
+        assertThat(hello.sayThankYou("Toby")).isEqualTo("Thank You Toby");
+
+        Hello proxiedHello = new HelloUppercase(new HelloTarget());
+        assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+        assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+    }
+
+    @Test
+    public void dynamicProxy() {
+        Hello proxiedHello = (Hello)Proxy.newProxyInstance(getClass().getClassLoader(), new Class[] {Hello. class}, new UppercaseHandler(new HelloTarget()));
+        assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+        assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+    }
+
+    @Test
+    public void proxyFactoryBean() {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+        pfBean.addAdvice(new UppercaseAdvice());
+
+        Hello proxiedHello = (Hello)pfBean.getObject();
+        assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+        assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+    }
+
+    static class UppercaseAdvice implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            String ret = (String)invocation.proceed();
+            return ret.toUpperCase();
+        }
+    }
+
+    interface Hello {
+        String sayHello(String name);
+        String sayHi(String name);
+        String sayThankYou(String name);
+    }
+
+    static class HelloUppercase implements Hello {
+        Hello hello;
+
+        public HelloUppercase(Hello hello) {
+            this.hello = hello;
+        }
+
+        @Override
+        public String sayHello(String name) {
+            return hello.sayHello(name).toUpperCase();
+        }
+
+        @Override
+        public String sayHi(String name) {
+            return hello.sayHi(name).toUpperCase();
+        }
+
+        @Override
+        public String sayThankYou(String name) {
+            return hello.sayThankYou(name).toUpperCase();
+        }
+    }
+
+    static class HelloTarget implements Hello {
+        @Override
+        public String sayHello(String name) {
+            return "Hello " + name;
+        }
+
+        @Override
+        public String sayHi(String name) {
+            return "Hi " + name;
+        }
+
+        @Override
+        public String sayThankYou(String name) {
+            return "Thank You " + name;
+        }
+    }
+
+    static class UppercaseHandler implements InvocationHandler {
+        Object target;
+
+        public UppercaseHandler(Object target) {
+            this.target = target;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            Object ret = method.invoke(target, args);
+            if (ret instanceof String && method.getName().startsWith("say")) {
+                return ((String)ret).toUpperCase();
+            }
+            else {
+                return ret;
+            }
+        }
+    }
+}
