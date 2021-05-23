b03c9e6cbd95fd46cfc7c2880c4a4500a7c7d93f
nefertirii <nefertiri972@gmail.com>
2021-05-23 23:57:29 +0900

    Update transaction config
    트랜잭션 속성과 전략을 UserService에 적용
    서비스 계층을 트랜잭션이 시작되고 종료되는 경계로 선정
    다른 계층이나 모듈에서 DAO에 직접 접근하는 것을 차단하기 위해 UserService에 DAO 에 위임하는 메소드 추가
    서비스 빈에 적용되는 포인트컷 표현식 등록, 빈 아이디가 Service로 끝나는 모든 빈에 transactionAdvice 빈의 부가기능 적용
    트랜잭션 속성을 가진 트랜잭션 어드바이스 등록, get 은 읽기전용으로 설정하고 나머지는 디폴트 트랜잭션 속성으로 설정

--- src/main/java/com/example/tobyspring/user/service/UserService.java
+++ src/main/java/com/example/tobyspring/user/service/UserService.java
@@ -2,7 +2,14 @@ package com.example.tobyspring.user.service;
 
 import com.example.tobyspring.user.domain.User;
 
+import java.util.List;
+
 public interface UserService {
     void add(User user);
+    User get(String id);
+    List<User> getAll();
+    void deleteAll();
+    void update(User user);
+
     void upgradeLevels();
 }
