15dd7c7135e1b10ec1e7a4bfb58430ce862a4903
nefertirii <nefertiri972@gmail.com>
2021-05-24 19:42:28 +0900

    Add transaction annotation
    포인트컷 표현식과 트랜잭션 속성을 @Transaction 애노테이션으로 변경

--- src/main/java/com/example/tobyspring/user/service/UserService.java
+++ src/main/java/com/example/tobyspring/user/service/UserService.java
@@ -1,15 +1,20 @@
 package com.example.tobyspring.user.service;
 
 import com.example.tobyspring.user.domain.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 public interface UserService {
     void add(User user);
-    User get(String id);
-    List<User> getAll();
     void deleteAll();
     void update(User user);
-
     void upgradeLevels();
+
+    @Transactional(readOnly = true)
+    User get(String id);
+
+    @Transactional(readOnly = true)
+    List<User> getAll();
 }
