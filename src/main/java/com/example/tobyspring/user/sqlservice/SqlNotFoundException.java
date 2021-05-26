11b06c16b215f9baffc4fcc7739a82bdd146d5df
nefertirii <nefertiri972@gmail.com>
2021-05-26 22:20:13 +0900

    Add sql reader, sql registry interface
    SqlReader 는 외부 리소스에서 SQL을 읽어 SqlRegistry 에 등록하는 기능 담당
    SqlRegistry 는 SQL을 제공받아 등록해뒀다가 키로 검색해서 돌려주는 기능 담당

---
+++ src/main/java/com/example/tobyspring/user/sqlservice/SqlNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.tobyspring.user.sqlservice;
+
+public class SqlNotFoundException extends Exception {
+    public SqlNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/tobyspring/user/sqlservice/SqlReader.java
@@ -0,0 +1,5 @@
+package com.example.tobyspring.user.sqlservice;
+
+public interface SqlReader {
+    void read(SqlRegistry sqlRegistry);
+}
