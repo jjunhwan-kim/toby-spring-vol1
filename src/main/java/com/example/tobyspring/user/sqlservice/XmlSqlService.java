3fccb0f0e835f400b2bdd2426cc14af74b0fd3be
nefertirii <nefertiri972@gmail.com>
2021-05-26 23:54:22 +0900

    Add interface implementation
    SqlReader, SqlRegistry 인터페이스 구현을 XmlSqlService 클래스에 추가
    클래스 내에서 두 개의 인터페이스 타입 오브젝트에 의존하는 구조로 구현
    sqlMap 은 sqlRegistry 에서만 사용
    sqlmapFile 은 sqlReader 에서만 사용

--- src/main/java/com/example/tobyspring/user/sqlservice/XmlSqlService.java
+++ src/main/java/com/example/tobyspring/user/sqlservice/XmlSqlService.java
@@ -11,16 +11,56 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-public class XmlSqlService implements SqlService {
+public class XmlSqlService implements SqlService, SqlRegistry, SqlReader {
     private Map<String, String> sqlMap = new HashMap<>();
     private String sqlmapFile;
+
+    private SqlReader sqlReader;
+    private SqlRegistry sqlRegistry;
 
     public void setSqlmapFile(String sqlmapFile) {
         this.sqlmapFile = sqlmapFile;
     }
 
+    public void setSqlReader(SqlReader sqlReader) {
+        this.sqlReader = sqlReader;
+    }
+
+    public void setSqlRegistry(SqlRegistry sqlRegistry) {
+        this.sqlRegistry = sqlRegistry;
+    }
+
     @PostConstruct
     public void loadSql() {
+        sqlReader.read(sqlRegistry);
+    }
+
+    @Override
+    public String getSql(String key) throws SqlRetrievalFailureException {
+        try {
+            return sqlRegistry.findSql(key);
+        }
+        catch (SqlNotFoundException e) {
+            throw new SqlRetrievalFailureException(e);
+        }
+    }
+
+    @Override
+    public void registerSql(String key, String sql) {
+        sqlMap.put(key, sql);
+    }
+
+    @Override
+    public String findSql(String key) throws SqlNotFoundException {
+        String sql = sqlMap.get(key);
+        if (sql == null)
+            throw new SqlNotFoundException(key + "에 대한 SQL을 찾을 수 없습니다");
+        else
+            return sql;
+    }
+
+    @Override
+    public void read(SqlRegistry sqlRegistry) {
         String contextPath = Sqlmap.class.getPackage().getName();
 
         try {
@@ -30,20 +70,11 @@ public class XmlSqlService implements SqlService {
             Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
 
             for (SqlType sql : sqlmap.getSql()) {
-                sqlMap.put(sql.getKey(), sql.getValue());
+                sqlRegistry.registerSql(sql.getKey(), sql.getValue());
             }
         }
         catch (JAXBException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    @Override
-    public String getSql(String key) throws SqlRetrievalFailureException {
-        String sql = sqlMap.get(key);
-        if (sql == null)
-            throw new SqlRetrievalFailureException(key + "를 이용해서 SQL을 찾을 수 없습니다");
-        else
-            return sql;
     }
 }
