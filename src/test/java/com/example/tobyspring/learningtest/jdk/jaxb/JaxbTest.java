07e94a0d1eacee7a539dd642c0cbd86b11d6371c
nefertirii <nefertiri972@gmail.com>
2021-05-26 19:11:09 +0900

    Add JAXB API learning test
    SQL 을 저장하는 독립적인 XML 문서 생성
    XML 문서의 구조를 정의하는 스키마 생성(sqlmap.xsd)
    JAXB 컴파일러로 스키마를 컴파일하여 XML 문서 바인딩용 클래스 생성
    XML 문서가 오브젝트로 전환되는 학습 테스트 추가

---
+++ src/test/java/com/example/tobyspring/learningtest/jdk/jaxb/JaxbTest.java
@@ -0,0 +1,35 @@
+package com.example.tobyspring.learningtest.jdk.jaxb;
+
+import com.example.tobyspring.user.sqlservice.jaxb.SqlType;
+import com.example.tobyspring.user.sqlservice.jaxb.Sqlmap;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JaxbTest {
+    @Test
+    public void readSqlmap() throws JAXBException {
+        String contextPath = Sqlmap.class.getPackage().getName();
+        JAXBContext context = JAXBContext.newInstance(contextPath);
+
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+
+        Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(getClass().getResourceAsStream("/test-sqlmap.xml"));
+
+        List<SqlType> sqlList = sqlmap.getSql();
+
+        assertThat(sqlList.size()).isEqualTo(3);
+        assertThat(sqlList.get(0).getKey()).isEqualTo("add");
+        assertThat(sqlList.get(0).getValue()).isEqualTo("insert");
+        assertThat(sqlList.get(1).getKey()).isEqualTo("get");
+        assertThat(sqlList.get(1).getValue()).isEqualTo("select");
+        assertThat(sqlList.get(2).getKey()).isEqualTo("delete");
+        assertThat(sqlList.get(2).getValue()).isEqualTo("delete");
+    }
+}
