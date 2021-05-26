cf6d509034da3eef59f10b0fe97ee7a0c55bec64
nefertirii <nefertiri972@gmail.com>
2021-05-26 19:49:44 +0900

    Improve xml sql service's initialization
    생성자에서 예외가 발생할 수 있는 초기화 작업을 다루지 않도록 수정
    loadSql 메소드를 @PostConstruct 애노테이션으로 설정, sqlService 빈의 오브젝트가 생성되고 DI 작업을 마친 뒤 호출됨
    XML 파일 경로를 외부에서 DI 받도록 sqlmapFile 프로퍼티 추가

--- src/main/java/com/example/tobyspring/user/sqlservice/XmlSqlService.java
+++ src/main/java/com/example/tobyspring/user/sqlservice/XmlSqlService.java
@@ -3,6 +3,7 @@ package com.example.tobyspring.user.sqlservice;
 import com.example.tobyspring.user.sqlservice.jaxb.SqlType;
 import com.example.tobyspring.user.sqlservice.jaxb.Sqlmap;
 
+import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -12,14 +13,20 @@ import java.util.Map;
 
 public class XmlSqlService implements SqlService {
     private Map<String, String> sqlMap = new HashMap<>();
+    private String sqlmapFile;
 
-    public XmlSqlService() {
+    public void setSqlmapFile(String sqlmapFile) {
+        this.sqlmapFile = sqlmapFile;
+    }
+
+    @PostConstruct
+    public void loadSql() {
         String contextPath = Sqlmap.class.getPackage().getName();
 
         try {
             JAXBContext context = JAXBContext.newInstance(contextPath);
             Unmarshaller unmarshaller = context.createUnmarshaller();
-            InputStream is = getClass().getResourceAsStream("/sqlmap.xml");
+            InputStream is = getClass().getResourceAsStream(sqlmapFile);
             Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
 
             for (SqlType sql : sqlmap.getSql()) {
