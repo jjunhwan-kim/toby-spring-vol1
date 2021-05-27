96b8ad85edf64a18402a0307fb6ab0dc4f711ced
nefertirii <nefertiri972@gmail.com>
2021-05-27 19:43:16 +0900

    Add oxm service abstraction learning test
    OXM 추상화 서비스 인터페이스 Unmarshaller 학습 테스트 추가
    Unmarshaller 의 구현 클래스로 Jaxb2Marshaller 사용

---
+++ src/test/java/com/example/tobyspring/learningtest/spring/oxm/OxmTest.java
@@ -0,0 +1,42 @@
+package com.example.tobyspring.learningtest.spring.oxm;
+
+import com.example.tobyspring.user.sqlservice.jaxb.SqlType;
+import com.example.tobyspring.user.sqlservice.jaxb.Sqlmap;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "/OxmTest-context.xml")
+public class OxmTest {
+    @Autowired
+    Unmarshaller unmarshaller;
+
+    @Test
+    public void unmarshallSqlMap() throws IOException {
+        Source xmlSource = new StreamSource(getClass().getResourceAsStream("/test-sqlmap.xml"));
+
+        Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(xmlSource);
+
+        List<SqlType> sqlList = sqlmap.getSql();
+
+        assertThat(sqlList.size()).isEqualTo(3);
+        assertThat(sqlList.get(0).getKey()).isEqualTo("add");
+        assertThat(sqlList.get(0).getValue()).isEqualTo("insert");
+        assertThat(sqlList.get(1).getKey()).isEqualTo("get");
+        assertThat(sqlList.get(1).getValue()).isEqualTo("select");
+        assertThat(sqlList.get(2).getKey()).isEqualTo("delete");
+        assertThat(sqlList.get(2).getValue()).isEqualTo("delete");
+    }
+}
