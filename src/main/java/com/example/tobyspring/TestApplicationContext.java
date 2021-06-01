13319f62cb54c51d06db70ac62ee7bc9741f04b1
nefertirii <nefertiri972@gmail.com>
2021-06-02 01:07:50 +0900

    Change test application context config
    DataSource 와 TransactionManager 빈 설정을 XML 에서 자바 코드로 변경

--- src/main/java/com/example/tobyspring/TestApplicationContext.java
+++ src/main/java/com/example/tobyspring/TestApplicationContext.java
@@ -1,9 +1,33 @@
 package com.example.tobyspring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ImportResource("/test-applicationContext.xml")
 public class TestApplicationContext {
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost/testdb");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        DataSourceTransactionManager tm = new DataSourceTransactionManager();
+        tm.setDataSource(dataSource());
+        return tm;
+    }
 }
