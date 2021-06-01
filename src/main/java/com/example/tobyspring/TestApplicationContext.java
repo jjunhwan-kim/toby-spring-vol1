3a8847d4efaf84756f48fcb9441a23afa8f502c4
nefertirii <nefertiri972@gmail.com>
2021-06-02 01:57:27 +0900

    Change test application context config
    EmbeddedDatabase 오브젝트 빈 설정 자바 코드로 변경
    <tx:annotation-driven /> 전용 태그 @EnableTransactionManagement 애노테이션으로 변경

--- src/main/java/com/example/tobyspring/TestApplicationContext.java
+++ src/main/java/com/example/tobyspring/TestApplicationContext.java
@@ -15,32 +15,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.mail.MailSender;
 import org.springframework.oxm.Unmarshaller;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 @Configuration
-@ImportResource("/test-applicationContext.xml")
+@EnableTransactionManagement
+//@ImportResource("/test-applicationContext.xml")
 public class TestApplicationContext {
-    @Autowired
-    SqlService sqlService;
-
     @Bean
     public DataSource dataSource() {
-        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-
-        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
-        dataSource.setUrl("jdbc:mysql://localhost/testdb");
-        dataSource.setUsername("root");
-        dataSource.setPassword("");
-
-        return dataSource;
+        SimpleDriverDataSource ds = new SimpleDriverDataSource();
+        ds.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+        ds.setUrl("jdbc:mysql://localhost/testdb");
+        ds.setUsername("root");
+        ds.setPassword("");
+        return ds;
     }
 
     @Bean
@@ -54,7 +54,7 @@ public class TestApplicationContext {
     public UserDao userDao() {
         UserDaoJdbc dao = new UserDaoJdbc();
         dao.setDataSource(dataSource());
-        dao.setSqlService(sqlService);
+        dao.setSqlService(sqlService());
         return dao;
     }
 
@@ -88,13 +88,10 @@ public class TestApplicationContext {
         return sqlService;
     }
 
-    @Resource
-    DataSource embeddedDatabase;
-
     @Bean
     public SqlRegistry sqlRegistry() {
         EmbeddedDbSqlRegistry sqlRegistry = new EmbeddedDbSqlRegistry();
-        sqlRegistry.setDataSource(embeddedDatabase);
+        sqlRegistry.setDataSource(embeddedDatabase());
         return sqlRegistry;
     }
 
@@ -105,4 +102,12 @@ public class TestApplicationContext {
         return marshaller;
     }
 
+    @Bean
+    public DataSource embeddedDatabase() {
+        return new EmbeddedDatabaseBuilder()
+                .setName("embeddedDatabase")
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:sqlRegistrySchema.sql")
+                .build();
+    }
 }
